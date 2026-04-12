86a5af74cbd738c914b2b4a9ccacd5ff0ffa62e8
guiellefranco <guielle.franco@gmail.com>
2026-04-12 18:22:13 -0400

    Added score state variables, and enhanced look

--- Lab1_Guielle_Franco/ContentView.swift
+++ Lab1_Guielle_Franco/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var currentNumber: Int = Int.random(in: 1...100)
+    @State private var correctAnswers: Int = 0
+    @State private var wrongAnswers: Int = 0
+    @State private var totalAttempts: Int = 0
     
     var body: some View {
         VStack(spacing: 30) {
-            Text("Prime Number Game")
+            Spacer()
             
             Text("\(currentNumber)")
                 .font(.system(size: 64, weight: .light, design: .serif))
@@ -21,9 +23,15 @@ struct ContentView: View {
             
             Button("Prime") {
             }
+            .font(.title2)
+            .foregroundColor(.mint)
             
             Button("Not Prime") {
             }
+            .font(.title2)
+            .foregroundColor(.mint)
+            
+            Spacer()
         }
         .padding()
     }
